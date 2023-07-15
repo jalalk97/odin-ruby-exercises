b8d8de5d892df60b274642f25bd34fd748146089
Jalal <jalal.kalyati@gmail.com>
2023-07-15 06:39:49 -0400

    Implement prepend method

--- linked_lists/lib/linked_list.rb
+++ linked_lists/lib/linked_list.rb
@@ -20,6 +20,16 @@ class LinkedList
     new_node.next_node = tail
     tail.prev_node = new_node
   end
+
+  def prepend(value)
+    new_node = Node.new(value)
+    next_node = head.next_node
+
+    next_node.prev_node = new_node
+    new_node.next_node =  next_node
+    new_node.prev_node = head
+    head.next_node = new_node
+  end
   # This class represents a node in the list
   class Node
     attr_accessor :value, :next_node, :prev_node
--- linked_lists/spec/linked_list_spec.rb
+++ linked_lists/spec/linked_list_spec.rb
@@ -22,4 +22,25 @@ RSpec.describe LinkedList do
       expect(list.tail.prev_node.value).to eql(2)
     end
   end
+
+  describe "prepend" do
+    let(:list) { LinkedList.new }
+
+    it "when the list is empty" do
+      value = 1
+      list.prepend(value)
+
+      expect(list.head.next_node.value).to eql(value)
+      expect(list.tail.prev_node.value).to eql(value)
+    end
+
+    it "when the list is not empty" do
+      list.append(1)
+      list.append(3)
+      list.prepend(2)
+
+      expect(list.head.next_node.value).to eql(2)
+      expect(list.tail.prev_node.value).to eql(3)
+    end
+  end
 end
