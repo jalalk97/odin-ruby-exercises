0352706e9d08e77db75b264fb061503605259745
Jalal <jalal.kalyati@gmail.com>
2023-07-15 06:38:36 -0400

    Implement append method

--- linked_lists/lib/linked_list.rb
+++ linked_lists/lib/linked_list.rb
@@ -2,13 +2,32 @@
 
 # This class represents a doubly linked list
 class LinkedList
-  def initialize; end
+  attr_reader :head, :tail
 
+  def initialize
+    @head = Node.new
+    @tail = Node.new
+    head.next_node = tail
+    tail.prev_node = head
+  end
+
+  def append(value)
+    new_node = Node.new(value)
+    prev_node = tail.prev_node
+
+    prev_node.next_node = new_node
+    new_node.prev_node = prev_node
+    new_node.next_node = tail
+    tail.prev_node = new_node
+  end
   # This class represents a node in the list
   class Node
-    def initialize(value = nil, next_node = nil)
+    attr_accessor :value, :next_node, :prev_node
+
+    def initialize(value = nil, next_node = nil, prev_node = nil)
       @value = value
       @next_node = next_node
+      @prev_node = prev_node
     end
   end
 end
--- linked_lists/spec/linked_list_spec.rb
+++ linked_lists/spec/linked_list_spec.rb
@@ -3,4 +3,23 @@
 require "linked_list"
 
 RSpec.describe LinkedList do
+  describe "append" do
+    let(:list) { LinkedList.new }
+
+    it "when the list is empty" do
+      value = 1
+      list.append(value)
+
+      expect(list.head.next_node.value).to eql(value)
+      expect(list.tail.prev_node.value).to eql(value)
+    end
+
+    it "when the list is not empty" do
+      list.append(1)
+      list.append(2)
+
+      expect(list.head.next_node.value).to eql(1)
+      expect(list.tail.prev_node.value).to eql(2)
+    end
+  end
 end
