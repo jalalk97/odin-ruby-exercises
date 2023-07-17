3160f9c256ed35a77ac5f5270577ca4cdc0c6c6e
Jalal <jalal.kalyati@gmail.com>
2023-07-17 05:37:24 -0400

    Define Tree class with a contructor and a pretty_print method

--- binary_search_trees/lib/bst.rb
+++ binary_search_trees/lib/bst.rb
@@ -1,3 +1,26 @@
+# frozen_string_literal: true
+
+# This class represents a balanced binary search tree.
+class Tree
+  attr_reader :root
+
+  def initialize(array)
+    @root = build_tree(array.uniq.sort)
+  end
+
+  def build_tree(array)
+    return nil if array.empty?
+
+    mid = (array.length - 1) / 2
+    Node.new(array[mid], build_tree(array[...mid]), build_tree(array[mid + 1..]))
+  end
+
+  def pretty_print(node = @root, prefix = "", is_left: true)
+    pretty_print(node.right, "#{prefix}#{is_left ? "│   " : "    "}", is_left: false) if node.right
+    puts "#{prefix}#{is_left ? "└── " : "┌── "}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? "    " : "│   "}", is_left: true) if node.left
+  end
+
   # This class represents a node in a binary search tree. Each node has most two possibly nil children of type Node
   # represented by the instance variables left and right.
   class Node
@@ -14,3 +37,4 @@
       data <=> other.data
     end
   end
+end
---
+++ binary_search_trees/spec/bst_spec.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+
+require "bst"
+
+RSpec.describe Tree do
+  describe "#initialize" do
+    context "when the input array is sorted and doesn't contain duplicates" do
+      it "creates the correct tree from (1..7)" do
+        tree = Tree.new((1..7).to_a)
+
+        expect(tree.root.data).to eql(4)
+        expect(tree.root.left.data).to eql(2)
+        expect(tree.root.left.left.data).to eql(1)
+        expect(tree.root.left.right.data).to eql(3)
+        expect(tree.root.right.data).to eql(6)
+        expect(tree.root.right.left.data).to eql(5)
+        expect(tree.root.right.right.data).to eql(7)
+      end
+
+      it "creates the correct tree from (1..4)" do
+        tree = Tree.new((1..4).to_a)
+
+        expect(tree.root.data).to eql(2)
+        expect(tree.root.left.data).to eql(1)
+        expect(tree.root.right.data).to eql(3)
+        expect(tree.root.right.right.data).to eql(4)
+      end
+
+      it "creates the correct tree from (1..9)" do
+        tree = Tree.new((1..9).to_a)
+
+        expect(tree.root.data).to eql(5)
+        expect(tree.root.left.data).to eql(2)
+        expect(tree.root.left.left.data).to eql(1)
+        expect(tree.root.left.right.data).to eql(3)
+        expect(tree.root.left.right.right.data).to eql(4)
+        expect(tree.root.right.data).to eql(7)
+        expect(tree.root.right.left.data).to eql(6)
+        expect(tree.root.right.right.data).to eql(8)
+        expect(tree.root.right.right.right.data).to eql(9)
+      end
+    end
+
+    context "when the input array contains duplicates" do
+      it "creates the correct tree with duplicates removed" do
+        tree = Tree.new([1, 1, 2, 2, 3, 3, 4, 4])
+
+        expect(tree.root.data).to eql(2)
+        expect(tree.root.left.data).to eql(1)
+        expect(tree.root.right.data).to eql(3)
+        expect(tree.root.right.right.data).to eql(4)
+      end
+    end
+
+    context "when the input array is not sorted" do
+      it "creates the correct tree" do
+        tree = Tree.new([1, 4, 3, 2, 6, 8, 9, 7, 5])
+
+        expect(tree.root.data).to eql(5)
+        expect(tree.root.left.data).to eql(2)
+        expect(tree.root.left.left.data).to eql(1)
+        expect(tree.root.left.right.data).to eql(3)
+        expect(tree.root.left.right.right.data).to eql(4)
+        expect(tree.root.right.data).to eql(7)
+        expect(tree.root.right.left.data).to eql(6)
+        expect(tree.root.right.right.data).to eql(8)
+        expect(tree.root.right.right.right.data).to eql(9)
+      end
+    end
+
+    context "when the input array is not sorted and contains duplicates" do
+      it "creates the correct tree" do
+        tree = Tree.new([1, 3, 2, 5, 7, 6, 4, 4, 2, 1, 3, 6, 5, 7])
+
+        expect(tree.root.data).to eql(4)
+        expect(tree.root.left.data).to eql(2)
+        expect(tree.root.left.left.data).to eql(1)
+        expect(tree.root.left.right.data).to eql(3)
+        expect(tree.root.right.data).to eql(6)
+        expect(tree.root.right.left.data).to eql(5)
+        expect(tree.root.right.right.data).to eql(7)
+      end
+    end
+  end
+end
