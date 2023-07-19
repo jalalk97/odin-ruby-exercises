a3bf6640fbda1d66df117188ebf0d3a8b97419d7
Jalal <jalal.kalyati@gmail.com>
2023-07-19 04:19:33 -0400

    Create Board class with a place_token method

--- connect_four/lib/connect_four.rb
+++ connect_four/lib/connect_four.rb
@@ -4,4 +4,34 @@ require "colorize"
 
 # Encapsulates all of the Connect Four game rules, logic and gameplay
 module ConnectFour
+  class IllegalMoveError < StandardError
+  end
+
+  module Color
+    YELLOW = 0
+    RED = 1
+  end
+
+  # Represents a Connect Four board
+  class Board
+    N_ROWS = 6
+    N_COLUMNS = 7
+
+    attr_reader :board
+
+    def initialize
+      @board = Array.new(N_COLUMNS) { Array.new(N_ROWS) }
+      @column_levels = Array.new(N_COLUMNS, 0)
+    end
+
+    def place_token(column, color)
+      columns_index = column - 1
+
+      raise IllegalMoveError, "Column #{column} is outside the board" unless column.between?(1, N_COLUMNS)
+      raise IllegalMoveError, "Column #{column} is full" unless column_levels[columns_index] < N_ROWS
+
+      board[columns_index][column_levels[columns_index]] = color
+      column_levels[columns_index] += 1
+    end
+  end
 end
--- connect_four/spec/connect_four_spec.rb
+++ connect_four/spec/connect_four_spec.rb
@@ -3,4 +3,39 @@
 require "connect_four"
 
 describe ConnectFour::Board do
+  let(:board) { ConnectFour::Board.new }
+
+  describe "#place_token" do
+    context "when the move is legal" do
+      it "updates the right column of the board" do
+        column = 1
+        color = ConnectFour::Color::YELLOW
+
+        board.place_token(column, color)
+
+        expect(board.board[column - 1][0]).to eql(color)
+      end
+    end
+
+    context "when the move is illegal" do
+      it "fails if the column is full" do
+        column = 1
+        color = ConnectFour::Color::YELLOW
+        ConnectFour::Board::N_ROWS.times { board.place_token(column, color) }
+
+        expect do
+          board.place_token(column, color)
+        end.to raise_error(ConnectFour::IllegalMoveError, "Column #{column} is full")
+      end
+
+      it "fails if the column is invalid" do
+        column = ConnectFour::Board::N_COLUMNS + 1
+        color = ConnectFour::Color::YELLOW
+
+        expect do
+          board.place_token(column, color)
+        end.to raise_error(ConnectFour::IllegalMoveError, "Column #{column} is outside the board")
+      end
+    end
+  end
 end
