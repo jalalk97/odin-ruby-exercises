afd609327a6a9f0b8867219e2ebfeef8a841676d
Jalal <jalal.kalyati@gmail.com>
2023-07-19 04:16:21 -0400

    Create initial project structure

---
+++ connect_four/lib/connect_four.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require "colorize"
+
+# Encapsulates all of the Connect Four game rules, logic and gameplay
+module ConnectFour
+end
---
+++ connect_four/spec/connect_four_spec.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require "connect_four"
+
+describe ConnectFour::Board do
+end
