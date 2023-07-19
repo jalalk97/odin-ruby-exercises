91ca1fd1b7200cf2295c3843bd54a4e7b0fba42b
Jalal <jalal.kalyati@gmail.com>
2023-07-19 04:20:00 -0400

    Implement to_s method

--- connect_four/lib/connect_four.rb
+++ connect_four/lib/connect_four.rb
@@ -33,5 +33,35 @@ module ConnectFour
       board[columns_index][column_levels[columns_index]] = color
       column_levels[columns_index] += 1
     end
+
+    def to_s
+      width = 2 * N_COLUMNS - 1
+
+      border = add_border("=" * (width + 2), 0)
+      title = add_border("CONNECT  FOUR".center(width))
+      content = pretty_string
+      labels = add_border((1..N_COLUMNS).to_a.join(" "))
+
+      [border, title, border, content, border, labels, border].join("\n")
+    end
+
+    private
+
+    def add_border(line, padding_amount = 1)
+      padding = " " * padding_amount
+      "|#{padding}#{line}#{padding}|"
+    end
+
+    def pretty_string
+      blank_circle = "\u25cb"
+      circle = "\u25cf"
+      colors = { Color::YELLOW => circle.yellow, Color::RED => circle.red, nil => blank_circle }
+
+      board.transpose.reverse.map do |line|
+        add_border(line.map { |cell| colors[cell] }.join(" "))
+      end.join("\n")
+    end
+
+    attr_accessor :column_levels
   end
 end
